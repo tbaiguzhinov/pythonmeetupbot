e7fb5fd2c7f7d9b8eb0ac09677d181f414e074f7
artemsav <studentbedn@gmail.com>
2022-07-27 16:59:28 +0300

    initial

--- bot/management/commands/bot.py
+++ bot/management/commands/bot.py
@@ -1,26 +1,73 @@
 import os
 
-from telegram import Update
-from telegram.ext import Updater, CommandHandler, CallbackContext
-
+from django.conf import settings
 from django.core.management.base import BaseCommand
-from dotenv import load_dotenv
+from telegram import Bot, InlineKeyboardButton, InlineKeyboardMarkup, Update
+from telegram.ext import (CallbackContext, CallbackQueryHandler,
+                          CommandHandler, ConversationHandler, Filters,
+                          MessageHandler, Updater)
+from bot.tg_bot import (
+    question_handle_menu,
+    program_handle_menu,
+    start,
+    flow_handle_menu,
+    flow_question_timeline,
+    handle_error,
+    end_conversation
+)
 
-
-def start(update: Update, context: CallbackContext):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Hello!")
+START, HANDLE_MENU, HANDLE_PROGRAMS,\
+    HANDLE_QUESTIONS, HANDLE_FLOW, CLOSE = range(6)
 
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        load_dotenv()
+        try:
+            start_bot()
+        except Exception as exc:
+            raise
 
-        updater = Updater(token=os.getenv("TELEGRAM_API_KEY"), use_context=True)
-        dispatcher = updater.dispatcher
 
-        start_handler = CommandHandler('start', start)
-        dispatcher.add_handler(start_handler)
-        
-        updater.start_polling()
-        updater.idle()
+def start_bot():
+    token = settings.TOKEN_TELEGRAM
+    user_id = settings.TG_USER_ID
+    logging_token = settings.TG_TOKEN_LOGGING
+    """Start the bot."""
+    updater = Updater(token)
+    dispatcher = updater.dispatcher
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler("start", start)],
+        states={
+            START: [
+                MessageHandler(Filters.text, start),
+                ],
+            HANDLE_MENU: [
+                CallbackQueryHandler(question_handle_menu, pattern="^(questions)$"),
+                CallbackQueryHandler(program_handle_menu, pattern="^(programs)$"),
+                CallbackQueryHandler(start, pattern="^(back)$"),
+                ],
+            HANDLE_FLOW: [
+                CallbackQueryHandler(start, pattern="^(back)$"),
+                CallbackQueryHandler(flow_handle_menu, pattern="^(flow)\d*$"),
+                CallbackQueryHandler(flow_question_timeline, pattern="^(timeline)$")
+            ],
+            HANDLE_PROGRAMS: [
+                CallbackQueryHandler(program_handle_menu, pattern="^(programs)\d*$"),
+                CallbackQueryHandler(start, pattern="^(back)$"),
+            ],
+            HANDLE_QUESTIONS: [
+                CallbackQueryHandler(question_handle_menu, pattern="^(questions)\d*$"),
+                CallbackQueryHandler(start, pattern="^(back)$"),
+            ],
+            CLOSE: [
+                
+            ]
+        },
+        fallbacks=[CommandHandler("end", end_conversation)],
+        name="my_conversation",
+    )
+    dispatcher.add_handler(conv_handler)
+    dispatcher.add_error_handler(handle_error)
+    updater.start_polling()
+    updater.idle()
