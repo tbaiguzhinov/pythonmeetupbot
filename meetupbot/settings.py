5d650988e868c6bd95019ad69532ad1902798b1c
tbaiguzhinov <tim.baiguzhinov@gmail.com>
2022-08-01 12:07:29 +0600

    Changed settings

--- meetupbot/settings.py
+++ meetupbot/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://pythonmeetup.herokuapp.com']
+ALLOWED_HOSTS = ['pythonmeetup.herokuapp.com']
 
 
 # Application definition
